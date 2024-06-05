475298aad8a5e4d8d195e067d90db8556f6964a0
PuruBro14 <gokuman0000@gmail.com>
2024-06-05 15:27:53 +0530

    after-payment-changes

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,23 +1,24 @@
-"module.exports = {
+module.exports = {
   apps: [
     {
-      name: 'backend',
-      script: 'index.js',
+      name: "backend",
+      script: "index.js",
       env: {
-        MONGODB_URL: 'mongodb+srv://user2407:user2407@cluster0.pjw5rbq.mongodb.net/CurrencyExchange',
+        MONGODB_URL:
+          "mongodb+srv://user2407:user2407@cluster0.pjw5rbq.mongodb.net/CurrencyExchange",
         PORT: 8100,
-        MAIL_HOST: 'smtp.gmail.com',
-        MAIL_USER: 'purusharma1405@gmail.com',
-        MAIL_PASS: 'ehic ixrg eadl lqjh',
-        JWT_SECRET: 'secret',
-        FOLDER_NAME: 'Media',
-        CLOUD_NAME: 'dbm49ofpr',
+        MAIL_HOST: "smtp.gmail.com",
+        MAIL_USER: "purusharma1405@gmail.com",
+        MAIL_PASS: "ehic ixrg eadl lqjh",
+        JWT_SECRET: "secret",
+        FOLDER_NAME: "Media",
+        CLOUD_NAME: "dbm49ofpr",
         API_KEY: 133882435375962,
-        API_SECRET: 'Kn8XaiBl2I4w_3X5jP2l9M-vgjc',
-        CLIENT_ID: 'TEST10191770356b0bd65101d6e3d1ea07719101',
-        CLIENT_SECRET: 'cfsk_ma_test_92343bedf3b0e95988bd61078376c370_4e7f49e2',
-        API_VERSION: '2023-03-01'
-      }
-    }
-  ]
-};"
+        API_SECRET: "Kn8XaiBl2I4w_3X5jP2l9M-vgjc",
+        CLIENT_ID: "TEST10191770356b0bd65101d6e3d1ea07719101",
+        CLIENT_SECRET: "cfsk_ma_test_92343bedf3b0e95988bd61078376c370_4e7f49e2",
+        API_VERSION: "2023-03-01",
+      },
+    },
+  ],
+};
