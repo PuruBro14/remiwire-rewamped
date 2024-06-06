291d4bb9e3ad09ddc9e35d0ee191d98491f4e2aa
PuruBro14 <gokuman0000@gmail.com>
2024-06-06 16:13:10 +0530

    after-payment-changes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -116,8 +116,9 @@ const Navbar = () => {
           </div>
 }
 
-          <div className="flex flex-col md:flex-row w-full justify-between items-start md:items-center gap-5 p-4">
+          <div className="flex flex-col md:flex-row w-full justify-between items-start md:items-center gap-5 p-4 md:h-24 md:mt-2">
             <nav>
+
               <ul className="flex md:flex-row flex-col gap-3 lg:gap-5 text-black text-sm">
                 {NavbarLinks?.map((ele, index) => {
                   if (ele?.title === "Settings" && !token) {
@@ -126,23 +127,73 @@ const Navbar = () => {
                   if (ele?.title === "My Orders" && !token) {
                     return null;
                   }
+                  if(showHamburger){
+                    return <li key={index} onClick={closeNavbar}>
+                      {ele?.title === "Services" ? (
+                        ''
+                      ) : (
+                        <Link to={ele?.title !== "Contact Us" ? ele?.path : ""}>
+                          <p
+                            onClick={(e) => {
+                              if (ele?.title === "Contact Us") {
+                                handleContactus(e);
+                              }
+                            }}
+                          >
+                            {ele.title}
+                          </p>
+                        </Link>
+                      )}
+                    </li>
+                  }
+                  else{
                   return (
                     <li key={index} onClick={closeNavbar}>
-                      <Link to={ele?.title !== "Contact Us" ? ele?.path : ""}>
-                        <p
-                          onClick={(e) => {
-                            if (ele?.title === "Contact Us") {
-                              handleContactus(e);
+                      {ele?.title === "Services" ? (
+                        <div>
+                          <div
+                            className="relative flex flex-row gap-2 items-center group"
+                            onClick={() =>
+                              setShowMobileServices(!showMobileServices)
                             }
-                          }}
-                        >
-                          {ele.title}
-                        </p>
-                      </Link>
+                          >
+                            <p>{ele?.title}</p>
+                            <IoIosArrowDropdownCircle />
+                            <div className="z-20 invisible absolute left-[50%] top-[0%] opacity-0 md:top-[50%] -translate-x-[50%] md:translate-y-[20%] flex flex-col rounded-md bg-richblack-5 p-4 text-richblack-900 transition-all duration-200 group-hover:visible group-hover:opacity-100 lg:w-[300px]">
+                              <div className="absolute left-[50%] translate-x-[80%] -translate-y-[45%] top-0 h-6 w-6  rounded rotate-45 bg-richblack-5"></div>
+                              {subLinksData?.map((ele, index) => {
+                                {
+                                }
+                                return (
+                                  <Link
+                                    className="p-2 border-b-2 no-underline hover:no-underline"
+                                    to={ele?.link}
+                                    key={index}
+                                  >
+                                    <p>{ele?.title}</p>
+                                  </Link>
+                                );
+                              })}
+                            </div>
+                          </div>
+                        </div>
+                      ) : (
+                        <Link to={ele?.title !== "Contact Us" ? ele?.path : ""}>
+                          <p
+                            onClick={(e) => {
+                              if (ele?.title === "Contact Us") {
+                                handleContactus(e);
+                              }
+                            }}
+                          >
+                            {ele.title}
+                          </p>
+                        </Link>
+                      )}
                     </li>
                   );
+                }
                 })}
-                {/* Show all services directly in mobile view */}
                 {showHamburger && subLinksData.map((subEle, subIndex) => (
                   <li key={subIndex} onClick={closeNavbar}>
                     <Link to={subEle?.link}>
@@ -151,6 +202,7 @@ const Navbar = () => {
                   </li>
                 ))}
               </ul>
+
             </nav>
 
             <div className="flex flex-col gap-y-4 md:flex-row md:gap-y-0 my-7 gap-x-1 lg:gap-x-4 items-center">
