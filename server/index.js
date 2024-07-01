e5fa12c6cd644de6155c5ec1ff87776ca4168326
PuruBro14 <gokuman0000@gmail.com>
2024-07-01 11:51:17 +0530

    added-our-achivements

--- server/index.js
+++ server/index.js
@@ -73,7 +73,7 @@ app.use("/api/v1",phoneOTPRoutes);
 app.get("/", (req, res) => {
   return res.json({
     success: true,
-    message: "Your server is up and runninggggggggggggggggggggggg....",
+    message: "Your server is up and running....",
   });
 });
 
